d38739f1804d3dd5d6c15ee6f1dd14f97cc33398
Jimmy <bruno_pier@hotmail.com>
2020-01-21 12:13:52 -0300

    Adding code to Updating State Immutably - Counter.js

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -17,7 +17,9 @@ class Counter extends Component {
                 <hr/>
                 <button onClick={this.props.onStoreResult}>Store Result</button>
                 <ul>
-                    <li onClick={this.props.onDeleteResult}></li>
+                    {this.props.storedResults.map(strResult => (
+                        <li key={strResult.id} onClick={this.props.onDeleteResult}>{strResult.value}</li>
+                    ))}
                 </ul>
             </div>
         );
@@ -26,7 +28,8 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
     return {
-            ctr: state.counter
+            ctr: state.counter,
+            storedResults: state.results
     };
 };
 
