6a780f77ad9e210a403fb4073479eba265d31927
Jimmy <bruno_pier@hotmail.com>
2020-01-21 12:13:28 -0300

    Adding code to Updating State Immutably - reducer.js

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -6,20 +6,28 @@ const initialState = {
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case 'INCREMENT':
-            return {
-                counter: state.counter + 1
-            }
+            const newState = Object.assign({}, state);
+            newState.counter = state.counter +1;
+            return newState;
         case 'DECREMENT':
             return {
+                ...state,
                 counter: state.counter - 1
             }
         case 'ADD':
             return {
+                ...state,
                 counter: state.counter + action.val
             }
         case 'SUBSTRACT':
             return {
+                ...state,
                 counter: state.counter - action.val
+            }
+        case 'STORE_RESULT':
+            return {
+                ...state,
+                results: state.results.concat({id: new Date(), value: state.counter})
             }
         }
     return state;
