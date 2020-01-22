54939fbce4e6a76f46a3ab207c3eaf5a35b37ca0
Jimmy <bruno_pier@hotmail.com>
2020-01-22 11:46:33 -0300

    Adding dependency - redux-thunk

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -4,3 +4,38 @@ export const ADD = 'ADD';
 export const SUBSTRACT = 'SUBSTRACT';
 export const STORE_RESULT = 'STORE_RESULT';
 export const DELETE_RESULT = 'DELETE_RESULT';
+
+const increment = () => {
+    return {
+        type: INCREMENT
+    };
+};
+const decrement = () => {
+    return {
+        type: DECREMENT
+    };
+};
+const add = (value) => {
+    return {
+        type: ADD,
+        val: value
+    };
+};
+const substract = (value) => {
+    return {
+        type: SUBSTRACT,
+        val: value
+    };
+};
+const storeResult = (res) => {
+    return {
+        type: STORE_RESULT,
+        result: res
+    };
+};
+const deleteResult = (res) => {
+    return {
+        type: DELETE_RESULT,
+        resultElId: res
+    };
+};
