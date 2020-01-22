f79f6efafa1c7d86762b9649a24ae5aaedd33649
Jimmy <bruno_pier@hotmail.com>
2020-01-22 14:20:22 -0300

    Reviewing code - object bug - dependencies - fix in progress

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -4,17 +4,17 @@ import * as actionCreators from '../../store/actions/index';
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
-class Counter extends Component {
 
+class Counter extends Component {
     render () {
         return (
             <div>
                 <CounterOutput value={this.props.ctr} />
                 <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
-                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter} />
-                <CounterControl label="Add 10" clicked={this.props.onAddCounter} />
-                <CounterControl label="Subtract 15" clicked={this.props.onSubstractCounter} />
-                <hr/>
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
+                <CounterControl label="Add 10" clicked={this.props.onAddCounter}  />
+                <CounterControl label="Subtract 15" clicked={this.props.onSubtractCounter}  />
+                <hr />
                 <button onClick={() => this.props.onStoreResult(this.props.ctr)}>Store Result</button>
                 <ul>
                     {this.props.storedResults.map(strResult => (
@@ -28,9 +28,10 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
     return {
-            ctr: state.ctr.counter,
-            storedResults: state.res.results
-    };
+
+        ctr: state.ctr.counter,
+        storedResults: state.res.results
+    }
 };
 
 const mapDispatchToProps = dispatch => {
