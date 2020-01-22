187388676ba5796144cb06cd88bee6b2e6d9a44c
Jimmy <bruno_pier@hotmail.com>
2020-01-22 11:52:29 -0300

    Adding imports -redux-thunk

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
 
 import './index.css';
 import App from './App';
@@ -27,7 +29,7 @@ const logger = store => {
 
 const composeEnahncers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(rootReducer, composeEnahncers(applyMiddleware(logger)));
+const store = createStore(rootReducer, composeEnahncers(applyMiddleware(logger, thunk)));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
